df5b3f8a1682cd6c6c950e28088dbe25a27c61b7
Jeroen <jtermaat@3dweb.nl>
2026-06-22 15:35:00 +0200

    Increment version to 1.0.4; declare compatibility with WordPress 7.0.

--- wp-content/plugins/3dweb-print-studio/3dweb-print-studio.php
+++ wp-content/plugins/3dweb-print-studio/3dweb-print-studio.php
@@ -7,7 +7,8 @@ Author: Jeroen Termaat
 Author URI: https://3dweb.nl
 Developer: Jeroen Termaat
 Developer URI: https://3dweb.nl
-Version: 1.0.3
+Version: 1.0.4
+Requires at least: 5.0
 Requires PHP: 7.4
 Last Modified: 2026-06-22
 License: GPLv2
@@ -21,7 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-$dweb_ps_version = '1.0.3';
+$dweb_ps_version = '1.0.4';
 define( 'DWEB_PS_VERSION', $dweb_ps_version );
 define( 'DWEB_PS_PATH', plugin_dir_path( __FILE__ ) );
 define( 'DWEB_PS_MAIN_URL', __FILE__ );
